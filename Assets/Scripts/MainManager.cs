194f0f5f079622dec57d8c5b97f9fad2eaacdb22
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-12-13 20:08:41 +0000

    Implement data persistence between sessions complete

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,9 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+
     // Start() and Update() methods deleted - we don't need them right now
 
     public static MainManager Instance;
@@ -22,6 +52,8 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
             DontDestroyOnLoad(gameObject);
+
+        LoadColor();
 
         }
     }
