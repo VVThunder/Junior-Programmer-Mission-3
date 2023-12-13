2764373a973365e5709325314733ff583ff9cf31
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-12-13 19:41:50 +0000

    Setup Sections Complete
    - Introduction to real-time 3D experience design
    - Set up version control
    - Explore the sample project
    - Principles of object-oriented programming
    - Create a scene flow

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEngine.SceneManagement;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -20,5 +24,19 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
